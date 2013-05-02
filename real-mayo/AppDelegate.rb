84972abba09bd2e59bffb0785bc5ec591a6e9d5d
adorechic <adorechic@gmail.com>
2013-05-02 10:55:17 -0700

    Change candidate as table view

--- real-mayo/AppDelegate.rb
+++ real-mayo/AppDelegate.rb
@@ -1,3 +1,4 @@
+require 'pathname'
 #
 #  AppDelegate.rb
 #  real-mayo
@@ -9,33 +10,49 @@
 class AppDelegate
     attr_accessor :window
     attr_accessor :textField
+    attr_accessor :tableView
+    attr_accessor :scrollView
+    
     def applicationDidFinishLaunching(a_notification)
         # Insert code here to initialize your application
+        size = NSSize.new(0.0, 0.0)
+        scrollView.setFrame(NSRect.new(scrollView.frame.origin, size))
+        p textField.frame.origin
     end
-    
-    def startTimer(sender)
-        if @timer.nil?
-            @time = 0.0
-            @timer = NSTimer
-            .scheduledTimerWithTimeInterval(0.1,
-                                            target: self,
-                                            selector: "timerHandler:",
-                                            userInfo: nil,
-                                            repeats: true)
+    def text(sender)
+        if @targets && !@targets.empty?
+            `open "/Applications/#{@targets.first}.app"`
         end
     end
     
-    def stopTimer(sender)
-        if @timer
-            @timer.invalidate
-            @timer = nil
+    def controlTextDidChange(a_notification)
+        if textField.stringValue.empty?
+            @targets = []
+        else
+            app_names = Pathname.glob('/Applications/*').
+                map { |path| path.basename.to_s.gsub(/\.app\Z/, '') }
+            @targets = app_names.select { |name| name =~ /#{textField.stringValue}/i }
+        end
+        point = NSPoint.new(15.0, textField.frame.origin.y - (20.0 * @targets.size))
+        size = NSSize.new(415.0, 20.0 * @targets.size)
+        
+        scrollView.setFrame(NSRect.new(point, size))
+        
+        tableView.reloadData
+        
+    end
+    
+    def numberOfRowsInTableView(aTableView)
+        # データが何行あるかを返す
+        if @targets
+            @targets.size
+        else
+            0
         end
     end
     
-    def timerHandler(obj)
-        @time += 0.1
-        string = sprintf("%.1f", @time)
-        textField.setStringValue(string)
+    def tableView(aTableView, objectValueForTableColumn: aTableColumn, row: rowIndex)
+        # テーブルに表示するデータを返す
+        @targets[rowIndex]
     end
 end
-
